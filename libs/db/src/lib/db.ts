e391763ec100d212a8a15da705a4d5334d1ec7ed
tar-aldev <frontdev.at@gmail.com>
2024-06-15 19:22:33 +0300

    chore: validate .env for db CU-86bz4h0n3

--- libs/db/src/lib/db.ts
+++ libs/db/src/lib/db.ts
@@ -4,7 +4,5 @@ import postgres from 'postgres';
 import * as schema from './schema';
 import { getPostgresDBCredentials } from './utils';
 
-// export const db = drizzle(sql, { schema });
-// for query purposes
 const queryClient = postgres(getPostgresDBCredentials());
 export const db = drizzle(queryClient, { schema });
--- libs/db/src/lib/drizzle.config.ts
+++ libs/db/src/lib/drizzle.config.ts
@@ -4,6 +4,5 @@ import { getPostgresDBCredentials } from './utils';
 export default defineConfig({
   schema: `${__dirname}/schema.ts`,
   dialect: 'postgresql',
-  // driver: 'pg',
   dbCredentials: getPostgresDBCredentials(),
 });
