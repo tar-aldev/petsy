2451d20d5fbf77db900d6e1c3eb98fcf0b2f68e5
tar-aldev <frontdev.at@gmail.com>
2024-06-15 19:22:33 +0300

    chore: validate .env for db CU-86bz4h0n3

--- libs/db/src/lib/utils.ts
+++ libs/db/src/lib/utils.ts
@@ -1,9 +1,20 @@
+import { z } from 'zod';
+
+const envVariablesSchema = z.object({
+  POSTGRES_HOST: z.string(),
+  POSTGRES_USER: z.string(),
+  POSTGRES_PASSWORD: z.string(),
+});
+
 export const getPostgresDBCredentials = () => {
+  const parsedVariables = envVariablesSchema.parse(process.env);
+  const { POSTGRES_HOST, POSTGRES_PASSWORD, POSTGRES_USER } = parsedVariables;
+
   return {
     database: 'petsy',
-    host: process.env['POSTGRES_HOST'],
+    host: POSTGRES_HOST,
     port: 5432,
-    user: process.env['POSTGRES_USER'],
-    password: process.env['POSTGRES_PASSWORD'],
+    user: POSTGRES_USER,
+    password: POSTGRES_PASSWORD,
   };
 };
