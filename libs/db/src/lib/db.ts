962ccccf97992a461871a80a36c592df228724bd
tar-aldev <frontdev.at@gmail.com>
2024-06-15 18:56:44 +0300

    chore: setup drizzle CU-86bz4h0n3

--- libs/db/src/lib/db.ts
+++ libs/db/src/lib/db.ts
@@ -4,5 +4,7 @@ import postgres from 'postgres';
 import * as schema from './schema';
 import { getPostgresDBCredentials } from './utils';
 
+// export const db = drizzle(sql, { schema });
+// for query purposes
 const queryClient = postgres(getPostgresDBCredentials());
 export const db = drizzle(queryClient, { schema });
