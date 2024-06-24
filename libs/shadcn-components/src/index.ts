194d9dea4715e8c48470c0e0c21891ad7ba8a954
tar-aldev <frontdev.at@gmail.com>
2024-06-24 22:02:07 +0300

    feat: extend Typography component CU-86bzavx57

--- libs/shadcn-components/src/index.ts
+++ libs/shadcn-components/src/index.ts
@@ -1,3 +1,5 @@
 // Use this file to export React client components (e.g. those with 'use client' directive) or other non-server utilities
 
 export * from './lib/components/ui/button';
+
+export { cn } from './lib/utils';
