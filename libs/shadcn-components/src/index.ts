ee1f4478713b49496e863563f028dc50f670f5e1
tar-aldev <frontdev.at@gmail.com>
2024-06-30 17:27:29 +0300

    feat(login): add markup for login page CU-86bzavx57

--- libs/shadcn-components/src/index.ts
+++ libs/shadcn-components/src/index.ts
@@ -1,5 +1,8 @@
 // Use this file to export React client components (e.g. those with 'use client' directive) or other non-server utilities
 
 export * from './lib/components/ui/button';
+export * from './lib/components/ui/badge';
+export * from './lib/components/ui/form';
+export * from './lib/components/ui/input';
 
 export { cn } from './lib/utils';
